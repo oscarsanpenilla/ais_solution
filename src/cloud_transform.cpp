e7f252d065af2dd410d23026aa5107eb815ca761
Oscar <oscarsan.penilla@gmail.com>
2019-09-28 02:47:22 -0700

    laser and pot cloud projected in the XY plane

---
+++ src/cloud_transform.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/PointCloud.h>
+#include <vector>
+
+float pot_pose_x;
+float pot_pose_y;
+float pot_pose_z;
+sensor_msgs::PointCloud pointCloud;
+
+void cloudCallback(const sensor_msgs::PointCloud msg)
+{
+    pointCloud = msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "cloud_transformer");
+    ros::NodeHandle node;
+
+    tf::TransformBroadcaster br;
+    tf::Transform transform;
+    tf::TransformListener listener;
+
+    ros::Subscriber cloud_sub = node.subscribe("pot_cloud", 100, cloudCallback);
+    ros::Publisher cloud_pub = node.advertise<sensor_msgs::PointCloud>("transformed_cloud",50);
+
+    ros::Rate rate(10.0);
+    while (node.ok())
+    {
+
+        sensor_msgs::PointCloud pointCloudTrans;
+        try
+        {
+
+        listener.transformPointCloud("world",pointCloud,pointCloudTrans);
+        cloud_pub.publish(pointCloudTrans);
+        }
+        catch (tf::TransformException ex)
+        {
+            ROS_ERROR("%s", ex.what());
+            ros::Duration(1.0).sleep();
+        }
+
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+};
--- src/solution2.cpp
+++ src/solution2.cpp
@@ -24,6 +24,10 @@ void poseCallback(const geometry_msgs::PoseStamped msg)
     // std::cout << msg << std::endl;
 }
 
+void cloudCallback(){
+
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "solution2");
@@ -37,20 +41,20 @@ int main(int argc, char **argv)
     ros::Rate rate(10.0);
     while (node.ok())
     {
-        transform.setOrigin(tf::Vector3(mrk_pose_x, mrk_pose_y, mrk_pose_z));
-        transform.setRotation(tf::Quaternion(mrk_ori_x, mrk_ori_y, mrk_ori_z, mrk_ori_w));
+        tf::Quaternion q;
+        double r, p, y;
+
+        transform.setOrigin(tf::Vector3(mrk_pose_x, mrk_pose_y, 0));
+        tf::Matrix3x3(tf::Quaternion(mrk_ori_x, mrk_ori_y, mrk_ori_z, mrk_ori_w)).getRPY(r,p,y);
+        q.setRPY(0,0,y);
+        transform.setRotation(q);
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "img_mrk"));
 
         transform.setOrigin(tf::Vector3(1.02,0,0));
         transform.setRotation(tf::Quaternion(0, 0, 0, 1));
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "img_mrk", "base_laser_link"));
 
-        // transform.setOrigin(tf::Vector3(0,0,0));
-        // transform.setRotation(tf::Quaternion(0, 0, 0, 1));
-        // br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_laser_link", "laser"));
-
         transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-        tf::Quaternion q;
         q.setRPY(3.1415, 0, 0);
         transform.setRotation(q);
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_laser_link", "laser"));
